93aee9e06627a72ac5b7a6252574eeb4fca58623
ljaramillom <jaramillo.lizet@gmail.com>
2019-10-14 17:41:28 -0500

    updated routes in app

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from "@angular/core";
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { NopagefoundComponent } from './nopagefound/nopagefound.component';
+import { HeaderComponent } from './header/header.component';
+import { BreadcrumbsComponent } from './breadcrumbs/breadcrumbs.component';
+
+@NgModule({
+    declarations: [
+        BreadcrumbsComponent,
+        HeaderComponent,
+        NopagefoundComponent,
+        SidebarComponent
+    ],
+    exports: [
+        BreadcrumbsComponent,
+        HeaderComponent,
+        NopagefoundComponent,
+        SidebarComponent
+    ]
+})
+export class SharedModule {}
