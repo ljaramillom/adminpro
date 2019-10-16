5732bbd1799f1036a2cd846422ee467ca163706a
ljaramillom <jaramillo.lizet@gmail.com>
2019-10-16 06:58:44 -0500

    add template for progress

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from "@angular/core";
+import { NgModule } from '@angular/core';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { ProgressComponent } from './progress/progress.component';
 import { Graficas1Component } from './graficas1/graficas1.component';
